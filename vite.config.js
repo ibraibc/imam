8444744357de85e86326221f1c7d5d3b3653987b
IbrahimAlomar <xdalibra@gmail.com>
2025-05-16 11:03:57 +0300

    changing vite config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,8 @@ import { fromJSON } from 'postcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  exports :{
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/imam/'
-      : '/'
-  },
+
+  publicPath: '/imam/',
   base: '/https://ibraibc.github.io/imam/',
   plugins: [vue()],
   resolve: {
